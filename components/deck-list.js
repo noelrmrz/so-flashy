e44e10ec32fe1e5ec006b8d09bd27075821a7e61
noelrmrz <noel.rmrz@gmail.com>
2021-05-19 13:34:15 -0700

    Added save and retrieve methods to api

--- components/deck-list.js
+++ components/deck-list.js
@@ -1,29 +1,33 @@
 import React, { Component } from 'react';
 import { SafeAreaView, View, FlatList, StyleSheet, StatusBar } from 'react-native';
 import { Text } from 'react-native-elements'
+import { getData, storeData } from '../utils/api'
 
 class DeckList extends Component {
 
-     DATA = [
-        {
+  state = {
+    decks: ''
+  }
+
+      componentDidMount() {
+        storeData([{
           id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
           title: 'First Item',
-        },
-        {
-          id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-          title: 'Second Item',
-        },
-        {
-          id: '58694a0f-3da1-471f-bd96-145571e29d72',
-          title: 'Third Item',
-        },
-      ]
+        }])
+
+          getData()
+            .then((results) => {
+              this.setState({
+                decks: results
+            })
+            })
+      }
 
     render() {
         return (
             <SafeAreaView style={styles.container}>
             <FlatList
-              data={this.DATA}
+              data={this.state.decks}
               renderItem={renderItem}
               keyExtractor={item => item.id}
             />
---
+++ utils/api.js
@@ -0,0 +1,21 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const STORAGE_KEY = 'So-Flasy:flashcards'
+
+export const getData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(STORAGE_KEY)
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch(e) {
+    // error reading value
+  }
+}
+
+export const storeData = async (value) => {
+  try {
+    const jsonValue = JSON.stringify(value)
+    await AsyncStorage.setItem(STORAGE_KEY, jsonValue)
+  } catch (e) {
+    // saving error
+  }
+}
