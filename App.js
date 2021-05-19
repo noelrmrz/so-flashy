6b9ccc91fa198cc4f7bf82533cc3ac04c912f09d
noelrmrz <noel.rmrz@gmail.com>
2021-05-18 20:06:39 -0700

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar'
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import DeckPreview from './components/deck-preview'
+import { Connect } from 'react-redux'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <DeckPreview />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/deck-preview.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { StyleSheet, TouchableOpacity, View } from "react-native"
+import { Text } from 'react-native-elements'
+
+class DeckPreview extends Component {
+
+    render() {
+        return (
+      <View style={styles.deck-preview}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this.onPress}
+        >
+          <Text h3>Deck Name</Text>
+          <Text h5>Deck Count</Text>
+        </TouchableOpacity>
+      </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+  deck-preview: {
+    backgroundColor: "black",
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 10
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+})
+
+export default DeckPreview
